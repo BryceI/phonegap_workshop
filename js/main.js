ae0eb3c23bdabd5713cfafc0d4b21435dd7039ee
Bryce Inouye <bryce@theinouyes.org>
2015-06-08 16:06:49 -0400

    employee view

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 var app = {
 
-
-
     showAlert: function(message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -29,16 +27,32 @@ var app = {
             $('body').on('mouseup', 'a', function(event) {
                 $(event.target).removeClass('tappable-active');
             });
+        };
+        $(window).on('hashchange', $.proxy(this.route, this));
+    },
+
+    route: function() {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
         }
     },
 
     initialize: function() {
         var self = this;
+        this.detailsURL = /^#employees\/(\d{1,})/;
+        this.registerEvents();
         this.store = new MemoryStore(function() {
                 // self.showAlert('Store Initialized', 'Info');
-                $('body').html(new HomeView(self.store).render().el)
+               self.route();
             });
-        this.registerEvents();
     }
 
 
